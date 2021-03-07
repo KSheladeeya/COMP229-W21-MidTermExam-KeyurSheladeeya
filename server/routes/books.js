ee0f3559fd0d4e66414c2ccfde1e270755c04127
Keyur Sheladeeya <kshelade@my.centennialcollege.ca>
2021-03-07 18:04:12 +0530

    added functionality of add book

--- server/routes/books.js
+++ server/routes/books.js
@@ -26,18 +26,42 @@ router.get('/', (req, res, next) => {
 //  GET the Book Details page in order to add a new Book
 router.get('/add', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  book.find( (err, books) => {
+    if (err) {
+      return console.error(err);
+    }
+    else {
+      res.render('books/details', {
+        title: 'Add Book',
+        books: books
+      });
+    }
+  });
 
 });
 
 // POST process the Book Details page and create a new Book - CREATE
 router.post('/add', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  let newBook = book({
+    "Title": req.body.title,
+    "Price": req.body.price,
+    "Author": req.body.author,
+    "Genre": req.body.genre
+});
+
+book.create(newBook, (err,book) =>{
+    if(err)
+    {
+        console.log(err);
+        res.end(err); // if error occurs, stop the server 
+    }
+    else
+    {
+        //refresh the book list
+        res.redirect('/books');
+    }
+});
 
 });
 
